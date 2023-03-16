ac56cb7d37dc6b344f64db89925cc1719df776a2
coderrick <derrick.duncan@temple.edu>
2023-03-15 20:27:29 -0400

    added axios, still updating scratchoffs to make reqs

--- src/pages/ScratchOffs.tsx
+++ src/pages/ScratchOffs.tsx
@@ -1,14 +1,22 @@
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import React from "react";
+import axios from "axios";
 
 const ScratchOffs = () => {
+
+  const getfirstBox = () => {
+    //   axios.get("https://jsonplaceholder.typicode.com/customers",()=>{
+
+    //   }).then(data: (any: any) => console.log(data.data)).catch(error => console.log(error));
+    // };
+  }
   return (
     <>
-    <h1>Scratch Offs</h1>
-    <br />
-    <div style={{display: "inline-flex"}}>
-    <Box sx={{ flexGrow: 1, display: "inline-block" }}>
+      <h1>Scratch Offs</h1>
+      <br />
+      <div style={{ display: "inline-flex" }}>
+        <Box sx={{ flexGrow: 1, display: "inline-block" }}>
           <Button style={{
             width: 225,
             height: 225,
@@ -19,7 +27,7 @@ const ScratchOffs = () => {
             boxShadow: "5px 10px #888888",
             backgroundColor: "blue"
           }}
-            href="/pageone"
+            onClick={()=>{console.log("made call")}}
             variant="outlined"><strong></strong></Button>
         </Box>
         <Box sx={{ flexGrow: 1 }}>
@@ -33,7 +41,6 @@ const ScratchOffs = () => {
             boxShadow: "5px 10px #888888",
             backgroundColor: "blue"
           }}
-            href="/pageone"
             variant="outlined"><strong></strong></Button>
         </Box>
         <Box sx={{ flexGrow: 1 }}>
@@ -47,10 +54,9 @@ const ScratchOffs = () => {
             boxShadow: "5px 10px #888888",
             backgroundColor: "blue"
           }}
-            href="/pageone"
             variant="outlined"><strong></strong></Button>
         </Box>
-        </div>
+      </div>
     </>
 
   );
