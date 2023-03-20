21047a33e006bfd2ebd4982ab0d8f68aa531a913
coderrick <derrick.duncan@temple.edu>
2023-03-20 17:56:09 -0400

    Added scratch-card lib and implemented component. Not working bc webpack config is needed for img load

--- src/pages/ScratchOffs.tsx
+++ src/pages/ScratchOffs.tsx
@@ -2,14 +2,22 @@ import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import React from "react";
 import axios from "axios";
+import ScratchCard from 'lesca-react-scratch-card';
+import CoverImage from 'src/img/scratch-off-bg.jpg';
 
 const ScratchOffs = () => {
 
   const getfirstBox = () => {
-    //   axios.get("https://jsonplaceholder.typicode.com/customers",()=>{
+    axios({
+      method: "get",
+      url: "https://www.google.com/"
+    }).then(response => {
+      const { data } = response;
+      console.info("data: ", data)
+    }).catch(error => {
+      console.error("req failed")
+    })
 
-    //   }).then(data: (any: any) => console.log(data.data)).catch(error => console.log(error));
-    // };
   }
   return (
     <>
@@ -17,18 +25,23 @@ const ScratchOffs = () => {
       <br />
       <div style={{ display: "inline-flex" }}>
         <Box sx={{ flexGrow: 1, display: "inline-block" }}>
-          <Button style={{
-            width: 225,
-            height: 225,
-            borderColor: "black",
-            color: "black",
-            borderRadius: 0,
-            margin: "1em",
-            boxShadow: "5px 10px #888888",
-            backgroundColor: "blue"
-          }}
-            onClick={()=>{console.log("made call")}}
-            variant="outlined"><strong></strong></Button>
+          <ScratchCard
+            cover={CoverImage}
+            percent={50}
+            width={225}
+            height={225}
+            onComplete={() => {
+              // ...script
+            }}
+          >
+            <div style={{
+              width: "100%",
+              height: "100%",
+              backgroundImage: `url(../img/cherry.png)`
+            }}>
+            </div>
+            {/* <img src="src/img/cherry.png" alt="cherry"  /> //To test local image load*/}
+          </ScratchCard>
         </Box>
         <Box sx={{ flexGrow: 1 }}>
           <Button style={{
